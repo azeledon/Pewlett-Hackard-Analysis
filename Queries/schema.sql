e38c2425261b2631785d205d8873b50d1350cb87
azeledon <valeriazrod3@gmail.com>
2023-01-19 08:29:26 -0700

    Add file

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -36,4 +36,25 @@ CREATE TABLE salaries (
 );
 
 
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+);
+
+
+CREATE TABLE titles(
+	emp_no INT NOT NULL,
+	title VARCHAR(30) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+	
+);
+
 SELECT * FROM departments;
